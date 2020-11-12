781d5249173972b6f2fa1ae8600b13beca9b97f8
Shivam Upadhyay <shivamup4545@gmail.com>
2020-11-12 13:07:13 +0530

    update upto 11-04-2018

---
+++ ssss.php
@@ -0,0 +1,116 @@
+<!doctypehtml>
+<html>
+<head>
+
+  <link rel="shortcut icon" href="b1.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+		h1{
+        background-color: powderblue;
+        background-size: 20px;
+        padding:5px;
+	}
+	*{
+		margin: 0px;
+		padding: 0
+	     }
+
+	.fa {
+  padding: 10px;
+  font-size: 30px;
+  width: 40px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+}
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-google {
+  background: #dd4b39;
+  color: white;
+}
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+
+#main{
+  width: 92%;
+    position: absolute;
+    left: 90px; 
+}
+#foot{
+  position: fixed;
+  top: 150px;
+  left: 0px;
+}
+#so{
+  list-style: none;
+}
+</style>
+</head>
+<body>
+  <h1 style="color:#990012"><font face="verdana" size="6px">Facebook CEO Mark Zuckerberg addresses privacy protections, gets quizzed on 'shadow profiles' and more in House Committee hearing</font></h1><br>
+  <div id="main">
+
+    <center><img src="j5.jpg" width="800px" height="400px"></center><br><br>
+<p><font face="cambria" size="4px">
+Mark Zuckerberg faced the lawmakers as part of the House Committee hearing regarding data protection and privacy of its users for the second day in a row.<br><br>
+
+Facebook CEO Mark Zuckerberg during his Day 1 testimony before the Senate Judiciary and Commerce Committees/ REUTERSFacebook CEO Mark Zuckerberg during his testimony before the Senate Judiciary and Commerce Committees. Image: Reuters
+There were a number of interesting things revealed during the discussion in the nearly five-hour hearing.<br><br>
+
+We went through the hearing and listed out five most significant topics in the exchange between the lawmakers and Facebook CEO. One significant point about today’s hearing in contrast to the hearing from yesterday was how the house members spent less time dwelling on Cambridge Analytica, in favour of focusing on a larger range of topics.<br><br>
+
+
+During the hearing, Facebook made a clear case about supporting General Data Privacy Regulation (GDPR) that EU introduced. Zuckerberg pointed out that Facebook will roll out ‘GDPR-style’ data protection for Facebook users globally because the regulations have “different important pieces”. This includes offering controls to users, asking for consent and more importantly, putting the controls in front of users. Facebook will also introduce a tool that will help users check their privacy settings for the apps that use Facebook authentication.<br><br>
+<center><img src="j6.jpg" width="800px" height="400px"></center><br>
+<h2><i>Tracking people on the internet regardless of having a Facebook account</i></h2>
+
+Committee members repeatedly questioned about how Facebook is tracking users around the internet using trackers regardless of the users holding an account with the company. Zuckerberg was also asked if Facebook maintained ‘Shadow profiles’ of internet users who never had an account or the ones who had deleted their Facebook accounts. The CEO added that the reason behind 'shadow profiles' was security, to keep a track of Facebook pages that an internet user was accessing without logging in.<br><br>Without that information, anyone could download all the information from all Facebook pages. The second reason was to serve relevant ads to visitors visiting Facebook. He added that he was not familiar with ‘shadow profiles’ and could not answer the scope of data that Facebook collected about Facebook users or non-users on the internet.
+<center><img src="j9.jpg" width="800px" height="400px"></center><br>
+
+<h2><i>Cambridge Analytica data leak affecting Zuckerberg’s own profile</i></h2>
+
+Mark Zuckerberg also revealed that data about his Facebook profile was also scraped by Cambridge Analytica making it more personal.<br><br>
+
+Congress members pushing Facebook for more privacy features and protection for minors<br><br>
+
+Zuckerberg was also asked about Facebook’s Messenger for Kids, about the provisions that the company takes to safeguard them. Lawmakers also asked that why was it's not possible to enforce a policy of ‘no-data-sharing’ until the user reaches 18 years of age. He sidestepped the question by answering on different lines, while not committing to adding any additional privacy protection for minors on the platform.<br><br>
+<center><img src="j8.jpg" width="800px" height="400px"></center><br>
+<h2><i>Fighting opioid sales on Facebook</i></h2>
+
+The last important topic during the hearing was that of opioids. Selling drugs on Facebook is against the terms and services of the platform in addition to being illegal. Committee members pointed out that a number of people were using Facebook to sell drugs on the platform. Zuckerberg responded by saying that the company will “try to improve its content moderation team” adding that “there are a number of areas of content that” Facebook needs to improve.<br><br>
+</font>
+</p></div><br><br><br>
+<div id="foot">
+  <ul id="so">
+<li><a href="https://www.google.co.in/search?q=fb&rlz=1C1SQJL_enIN772IN772&oq=fb&aqs=chrome..69i57l2j69i60l4.555j0j4&sourceid=chrome&ie=UTF-8" class="fa fa-facebook">&nbsp &nbsp</a></li>
+<li><a href="https://twitter.com/" class="fa fa-twitter"></a></li>
+<li><a href="https://www.google.co.in/" class="fa fa-google"></a></li>
+<li><a href="https://www.youtube.com/watch?v=DVYuPZlRWzc" class="fa fa-youtube"></a></li>
+<li><a href="https://www.instagram.com/?hl=en" class="fa fa-instagram"></a></li>
+</ul></div>
+</font>
+</body>
+</html>
